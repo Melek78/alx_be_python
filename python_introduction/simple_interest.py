f47984e7e03c57049ee11bf5474d0a1f395945a7
Melek78 <abemelek78@gmail.com>
2025-07-10 11:02:15 +0300

    simple_interest.py error fixing

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-principal = 1000 #representing 1000$
-rate = float(0.05) #representing 5% annual interest rate
-time = 3 #representing 3 years time
-interest = float(principal * rate * time)
-print("The simple interest is:", interest)
+P = 1000 #representing 1000$
+R = float(0.05) #representing 5% annual interest rate
+T = 3 #representing 3 years time
+I = float(P * R * T)
+print("The simple interest is:", I)
