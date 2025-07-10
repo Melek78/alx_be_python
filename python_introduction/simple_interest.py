32dbf6e504127bce2eb4e5bad2d59430707f0206
Melek78 <abemelek78@gmail.com>
2025-07-10 11:23:08 +0300

    error fixing

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000 #representing 1000$
-rate = (0.05) #representing 5% annual interest rate
+rate = 0.05 #representing 5% annual interest rate
 time = 3 #representing 3 years time
-interest = (principal * rate * time)
+interest = principal * rate * time
 print("The simple interest is:", interest)
