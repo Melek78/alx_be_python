20c3a76e7d45eb314ed707cea18c5bfcf07bec5b
Melek78 <abemelek78@gmail.com>
2025-07-10 10:50:19 +0300

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 #representing 1000$
+rate = float(0.05) #representing 5% annual interest rate
+time = 3 #representing 3 years time
+interest = float(principal * rate * time)
+print("The simple interest is:", interest)
