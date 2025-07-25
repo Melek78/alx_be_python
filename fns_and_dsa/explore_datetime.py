72ef343ecfd7c536419cc75b037a3ecb62a7e8f4
Melek78 <abemelek@Abemelek.localdomain>
2025-07-25 19:32:54 +0300

    fixing error

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,11 @@
 import datetime
 from datetime import timedelta
 def display_current_datetime():
-    current_date = datetime.date.today()
+    current_date = datetime.datetime.now()
     formatted_date_and_time = current_date.strftime("YYYY-MM-DD HH:MM:SS")
     print(formatted_date_and_time)
 def calculate_future_date():
     days = int(input("Enter number of days: "))
-    today = datetime.date.today()
+    today = datetime.datetime.now()
     future_date = today + timedelta(days)
     print(future_date.strftime("YYYY-MM-DD"))
