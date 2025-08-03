f4a943a02d30947b98a0f50b63d1e38c45f46a1b
Melek78 <abemelek@Abemelek.localdomain>
2025-08-03 16:33:20 +0300

    bank account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount():
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.amount = amount
+        self.account_balance += self.amount
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.account_balance >= self.amount:
+            self.account_balance -= self.amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
